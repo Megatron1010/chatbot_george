8c3f582a5d0fb6221cea2d2a50de72b67417f16b
Anga <atinzi@gmail.com>
2021-08-15 16:12:17 +0200

    Version 1

---
+++ app.py
@@ -0,0 +1,56 @@
+
+
+import pandas as pd
+items = {"1": "toilet", "2": "bathtub", "3": "sink", "4":" geyser", "5":" bathroom taps", "6": "kitchen taps", }
+jobs = {"1": "repair something", "2": "install something", "3": "replace something" }
+df = pd.DataFrame()
+jobList = []
+workList = []
+dict = {}
+counter = 0
+
+#def quoteMaker(jobList, workList)
+
+
+while counter == 0:
+    print("Good day, If you are looking for the services of a Plumber, you in the right place:\n")
+    user = input("Please enter your name:\n")
+
+    print("Hello \n", user)
+
+    counter += 1
+
+    print("What can we do for you:\n")
+while counter == 1:
+    print("Reply with the relevent number\n")
+    job = (input("1. repair something. 2. install something 3. replace something:\n"))
+
+    if job not in "123":
+        counter = 1
+
+    else:
+        jobList.append(jobs[job])
+        counter += 1
+
+    while counter == 2:
+        work = (input("1: toilet, 2: bathtub, 3: sink, 4: geyser, 5: bathroom taps, 6: kitchen taps:\n"))
+        if work not in "123456":
+            counter = 2
+
+        else:
+            workList.append(items[work])
+            counter += 1
+
+        while counter == 3:
+            add = input("1: get quote, 2: add more:\n")
+            if add not in "12":
+                counter = 3
+            elif add == "1":
+                dict.update({"customer":user, "job": jobList, "work": workList })
+                #df.append(dict, ignore_index = True)
+                counter += 1
+                for x in range(len(jobList)):
+
+                    print(dict['customer']+ ": " + dict["job"][x]+ ": " +dict["work"][x] )
+            else:
+                counter = 1
---
+++ facebookBot.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+import requests
+
+
+app = Flask(__name__)
+
+PAGE_ACCESS_TOKEN = "EAAMkZBZCiLpCABABXGgrsATop8I3X8gGc08R0XTOEsAB8CNkFkZBhDAdLAT7oGYGuZAKOc61EVKJB2JW88v3mfNFHhHnZBEjJVuvog7GhLcr5JZCUPqV3QOCPZBVtPO9iY0X02ns43r5rQ60IXIxiUyydZCWmRIa5damKwpBLzObRfJeegdLOjbo"
+FB_API_URL = "https://graph.facebook.com/v11.0/me/messages?access_token=<PAGE_ACCESS_TOKEN>""
+VERITY_TOKEN = "justDoIt"
+
+
+def get_bot_response(message):
+    """This is just a dummy function, returning a variation of what
+    the user said. Replace this function with one connected to chatbot."""
+    return "This is a dummy response to '{}'".format(message)
+
+
+def verify_webhook(req):
+    if req.args.get("hub.verify_token") == VERIFY_TOKEN:
+        return req.args.get("hub.challenge")
+    else:
+        return "incorrect"
+
+def respond(sender, message):
+    """Formulate a response to the user and
+    pass it on to a function that sends it."""
+    response = get_bot_response(message)
+    send_message(sender, response)
+
+
+def is_user_message(message):
+    """Check if the message is a message from the user"""
+    return (message.get('message') and
+            message['message'].get('text') and
+            not message['message'].get("is_echo"))
+
+
+@app.route("/webhook", methods=['GET','POST'])
+def listen():
+    """This is the main function flask uses to
+    listen at the `/webhook` endpoint"""
+    if request.method == 'GET':
+        return verify_webhook(request)
+
+    if request.method == 'POST':
+        payload = request.json
+        event = payload['entry'][0]['messaging']
+        for x in event:
+            if is_user_message(x):
+                text = x['message']['text']
+                sender_id = x['sender']['id']
+                respond(sender_id, text)
+
+        return "ok"
